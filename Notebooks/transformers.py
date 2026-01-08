85c991c6d431ebd9504d8f7cf9d4fe71a897ffe5
VictorMijangosDeLaCruz <118194155+VictorMijangosDeLaCruz@users.noreply.github.com>
2026-01-08 12:29:04 -0600

    Add files via upload

--- Notebooks/transformers.py
+++ Notebooks/transformers.py
@@ -2,34 +2,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 from collections import defaultdict
-<<<<<<< HEAD
-from sklearn.model_selection import train_test_split
-
-class MyDataset(torch.utils.data.Dataset):
-    def __init__(self, x, y, pad):
-        #Carga del csv
-        self.x = torch.tensor(nn.utils.rnn.pad_sequence(x, padding_value=pad)).T #x
-        self.y = torch.tensor(y)
-        #Tamaño de los datos
-        self.size = len(x)
-
-    def __len__(self):
-        #Regresa tamaño
-        return len(self.y)
-
-    def __getitem__(self,idx):
-        #Regresa un dato
-        return self.x[idx], self.y[idx]
-
-def get_dataset(x, y, pad=0, batch_size=128):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-
-    train_loader = torch.utils.data.DataLoader(MyDataset(x_train,y_train, pad=pad), batch_size=batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(MyDataset(x_test,y_test, pad=pad), batch_size=10, shuffle=False)
-
-    return train_loader, test_loader
-=======
->>>>>>> 0df15a53746840b38e75650741cae71447b43235
 
 def vocab():
     vocab = defaultdict()
@@ -43,51 +15,13 @@ def index(corpus, voc, split=False):
         else:
             pass
         yield torch.tensor([voc[w.lower()] for w in sent], dtype=torch.long)
-<<<<<<< HEAD
-        
-class SelfAttention(nn.Module):
-    def __init__(self, d_model):
-        super(SelfAttention, self).__init__()
-        # Capas de proyecciones
-        self.d_model = d_model
-        self.Q = nn.Linear(d_model, d_model, bias=False)
-        self.K  = nn.Linear(d_model, d_model, bias=False)
-        self.V  = nn.Linear(d_model, d_model, bias=False)
-        
-    def forward(self, x):
-        # Proyección de los datos
-        query,key,value = self.Q(x),self.K(x),self.V(x)
-        # Cálculo de pesos de atención
-        scores = torch.matmul(query, key.transpose(-1,-2))/np.sqrt(self.d_model)
-        p_attn = torch.nn.functional.softmax(scores, dim = -1)
-        #Suma ponderada
-        Vs = torch.matmul(p_attn, value).reshape(x.shape)
-        
-        return Vs, p_attn
-    
-=======
 
->>>>>>> 0df15a53746840b38e75650741cae71447b43235
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=1000, warmup=10000):
         super(PositionalEncoding, self).__init__()
         self.d_model = d_model
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len).unsqueeze(1)
-<<<<<<< HEAD
-        div_term = torch.exp(torch.arange(0, d_model, 2) * -(torch.log(torch.tensor(warmup)) / d_model))
-
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-                         
-        self.register_buffer('pe', pe)
-        
-    def forward(self, x):
-        batch_size, seq_len, _ = x.shape
-        pe_slice = self.pe[:seq_len, :self.d_model]
-
-        return pe_slice.unsqueeze(0).expand(batch_size, -1, -1)
-=======
         div_term = warmup**(2*torch.arange(0, d_model, 2)/d_model)
         pe[:, 0::2] = torch.sin(position / div_term)
         pe[:, 1::2] = torch.cos(position / div_term)
@@ -96,7 +30,6 @@ class PositionalEncoding(nn.Module):
     def forward(self, n):
         
         return self.pe[:n, :self.d_model]
->>>>>>> 0df15a53746840b38e75650741cae71447b43235
         
 class Encoding(nn.Module):
     def __init__(self, vocab_size, d_model, scale=True):
@@ -113,8 +46,6 @@ class Encoding(nn.Module):
             encoding = self.emb(x) + self.pe(x.size(0))
         
         return encoding
-<<<<<<< HEAD
-=======
         
 class SelfAttention(nn.Module):
     def __init__(self, d_model):
@@ -135,7 +66,6 @@ class SelfAttention(nn.Module):
         Vs = torch.matmul(p_attn, value).reshape(x.shape)
         
         return Vs, p_attn
->>>>>>> 0df15a53746840b38e75650741cae71447b43235
 
 class MaskAttention(nn.Module):
     #Atención enmascarando subsecuentes
